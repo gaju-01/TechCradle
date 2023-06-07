14c8299596ca9674fa701b41dd633a790c24f8d4
gaju-01 <you@example.com>
2023-06-07 15:30:23 +0530

    Initial Commit

---
+++ src/main/java/RESTAPI/SPRINGBOOTCP/SpringbootcpApplication.java
@@ -0,0 +1,13 @@
+package RESTAPI.SPRINGBOOTCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RESTAPI/SPRINGBOOTCP/SpringbootcpApplicationTests.java
@@ -0,0 +1,13 @@
+package RESTAPI.SPRINGBOOTCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
