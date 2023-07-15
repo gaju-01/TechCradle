caccdeb8e98f233d079de71ab4490b179af9738d
gaju-01 <gajananchegde01@gmail.com>
2023-07-15 23:24:18 +0530

    [LocalHost Refused To Connect Excelption Resolved]

--- SpringBoot/src/main/java/CP/REST/API/SpringBoot/Security/SecurityConfiguration.java
+++ SpringBoot/src/main/java/CP/REST/API/SpringBoot/Security/SecurityConfiguration.java
@@ -1,23 +1,33 @@
 package CP.REST.API.SpringBoot.Security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(
-                        auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest()
-                                .authenticated())
-                .httpBasic(withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf().disable().build();
-    }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(auth ->
+      auth
+        .requestMatchers(HttpMethod.OPTIONS, "/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+    );
+    http.httpBasic(withDefaults());
+    http.headers().frameOptions().disable();
+    http.sessionManagement(session ->
+      session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    );
+    http.csrf().disable();
+    return http.build();
+  }
 }
