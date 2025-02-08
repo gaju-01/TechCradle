d28bd98226830620f00eed34b682a257e05c2ac3
gaju-01 <you@example.com>
2025-02-08 13:16:11 +0530

    Allowed the vercel domain for requests

--- SpringBoot/src/main/java/CP/REST/API/SpringBoot/Application.java
+++ SpringBoot/src/main/java/CP/REST/API/SpringBoot/Application.java
@@ -19,7 +19,7 @@ public class Application {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("*")
-                        .allowedOrigins("http://localhost:3000/");
+                        .allowedOrigins("http://localhost:3000/", "https://tech-cradle-mgacz477q-gaju-01s-projects.vercel.app/");
             }
         };
     }
